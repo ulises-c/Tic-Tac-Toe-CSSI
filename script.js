6eed2e01dafe94f4c0759ce6f540aec04615413c
uli-oli <ez1ollie@yahoo.com>
2020-07-27 15:17:50 -0700

    added area locations

--- script.js
+++ script.js
@@ -1,62 +1,85 @@
-let area1, area2, area3, area4, area5, area6, area7, area8, area9;
+let a1, a2, a3, a4, a5, a6, a7, a8, a9;
 let a1Collide = false;
+let a2Collide = false;
+let a3Collide = false;
+let a4Collide = false;
+let a5Collide = false;
+let a6Collide = false;
+let a7Collide = false;
+let a8Collide = false;
+let a9Collide = false;
 
 function setup(){
     createCanvas(400, 400);
     areaLocations();
     test_circle = new Circle;
     test_cross = new Cross;
+    xImage = loadImage("https://i.imgur.com/a2GcPMe.png")
 }
 
 function areaLocations(){
-    area1 = {
+    a1 = {
         // top left
         x: width * 1/3,
         y: height * 1/3,
         xCenter: width * 1/6,
         yCenter: height * 1/6
     };
-    area2 = {
+    a2 = {
         // top middle
         x: width * 2/3,
         y: height * 1/3,
-        xCenter: width * 2/6,
+        xCenter: width * 3/6,
         yCenter: height * 1/6
     };
-    area3 = {
+    a3 = {
         // top right
         x: width,
-        y: height * 1/3
+        y: height * 1/3,
+        xCenter: width * 5/6,
+        yCenter: height * 1/6
     };
-    area4 = {
+    a4 = {
         // middle left
         x: width * 1/3,
-        y: height * 2/3
+        y: height * 2/3,
+        xCenter: width * 1/6,
+        yCenter: height * 3/6,
     };
-    area5 = {
+    a5 = {
         // center
         x: width * 2/3,
-        y: height * 2/3
+        y: height * 2/3,
+        xCenter: width * 3/6,
+        yCenter: height * 3/6
     }
-    area6 = {
+    a6 = {
         // middle right
         x: width,
-        y: height * 2/3
+        y: height * 2/3,
+        xCenter: width * 5/6,
+        yCenter: height * 3/6
     };
-    area7 = {
+    a7 = {
         // bottom left
         x: width * 1/3,
-        y: height
+        y: height,
+        xCenter: width * 1/6,
+        yCenter: height * 5/6
     };
-    area8 = {
+    a8 = {
         // bottom middle
         x: width * 2/3,
-        y: height
+        y: height,
+        xCenter: width * 3/6,
+        yCenter: height * 5/6
     };
-    area9 = {
+    a9 = {
         // bottom right
         x: width,
-        y: height
+        y: height,
+        xCenter: width * 5/6,
+        yCenter: height * 5/6
     };
 }
   
@@ -65,7 +88,21 @@ function draw(){
     ticTacToeBoard();
     // test_circle.display();
     // test_cross.display();
+    // image(xImage, 100, 100);
+    drawLocations();
     checkCollision();
+}
+
+function drawLocations(){
+    text("1", a1.xCenter, a1.yCenter);
+    text("2", a2.xCenter, a2.yCenter);
+    text("3", a3.xCenter, a3.yCenter);
+    text("4", a4.xCenter, a4.yCenter);
+    text("5", a5.xCenter, a5.yCenter);
+    text("6", a6.xCenter, a6.yCenter);
+    text("7", a7.xCenter, a7.yCenter);
+    text("8", a8.xCenter, a8.yCenter);
+    text("9", a9.xCenter, a9.yCenter);
 }
 
 function ticTacToeBoard(){
@@ -78,38 +115,91 @@ function ticTacToeBoard(){
 }
 
 function checkCollision() {
-    a1Collide = collidePointRect(mouseX, mouseY, 0, 0, area1.x, area1.y);
-    a2Collide = collidePointRect(mouseX, mouseY, area1.x, 0, area2.x, area1.y);
-    // console.log("Mouse pressed: ", mousePressed);
+    a1Collide = collidePointRect(mouseX, mouseY, 0, 0, a1.x, a1.y);
+    a2Collide = collidePointRect(mouseX, mouseY, a1.x, 0, a2.x, a2.y);
+    a3Collide = collidePointRect(mouseX, mouseY, a2.x, 0, a3.x, a3.y);
+
+    a4Collide = collidePointRect(mouseX, mouseY, 0, a1.y, a4.x, a4.y);
+    a5Collide = collidePointRect(mouseX, mouseY, a4.x, a2.y, a5.x, a5.y);
+    a6Collide = collidePointRect(mouseX, mouseY, a5.x, a3.y, a6.x, a6.y);
+
+    a7Collide = collidePointRect(mouseX, mouseY, 0, a4.y, a7.x, a7.y);
+    a8Collide = collidePointRect(mouseX, mouseY, a7.x, a5.y, a8.x, a8.y);
+    a9Collide = collidePointRect(mouseX, mouseY, a8.x, a6.y, a9.x, a9.y);
+
     if(a1Collide){
-        // test_circle.display(area1.xCenter, area1.yCenter);
+        test_circle.display(a1.xCenter, a1.yCenter);
         return a1Collide;
-    };
+    }
     if(a2Collide){
-        test_circle.display(area2.xCenter, area2.yCenter);
+        test_circle.display(a2.xCenter, a2.yCenter);
+        return a2Collide;
+    }
+    if(a2Collide){
+        test_circle.display(a3.xCenter, a3.yCenter);
+        return a3Collide;
+    }
+    if(a4Collide){
+        test_circle.display(a4.xCenter, a4.yCenter);
+        return a4Collide;
+    }
+    if(a5Collide){
+        test_circle.display(a5.xCenter, a5.yCenter);
+        return a5Collide;
+    }
+    if(a6Collide){
+        test_circle.display(a6.xCenter, a6.yCenter);
+        return a6Collide;
+    }
+    if(a7Collide){
+        test_circle.display(a7.xCenter, a7.yCenter);
+        return a7Collide;
+    }
+    if(a8Collide){
+        test_circle.display(a8.xCenter, a8.yCenter);
+        return a8Collide;
+    }
+    if(a9Collide){
+        test_circle.display(a9.xCenter, a9.yCenter);
+        return a9Collide;
     }
 }
 
 function mousePressed(){
     if (a1Collide){
-        // console.log("Area 1 collision: ", a1Collide);
+        console.log("Area 1 collision: ", a1Collide);
         test_cross.display();
-        // if (alreadyFilled){
-        //     return
-        // }
-        // else if(Player1Turn){
-        //     console.log('test')
-        // }
-        // else if (Player2Turn){
-        //     console.log('test')
-        // }
+    }
+    if (a2Collide){
+        console.log("Area 2 collision: ", a3Collide);
+    }
+    if (a3Collide){
+        console.log("Area 3 collision: ", a3Collide);
+    }
+    if(a4Collide){
+        console.log("Area 4 collision: ", a4Collide);
+    }
+    if(a5Collide){
+        console.log("Area 5 collision: ", a5Collide);
+    }
+    if(a6Collide){
+        console.log("Area 6 collision: ", a6Collide);
+    }
+    if(a7Collide){
+        console.log("Area 7 collision: ", a7Collide);
+    }
+    if(a8Collide){
+        console.log("Area 8 collision: ", a8Collide);
+    }
+    if(a9Collide){
+        console.log("Area 9 collision: ", a9Collide);
     }
 }
 
 class Cross {
     constructor(){
-        this.x = area1.xCenter;
-        this.y = area1.yCenter;
+        this.x = a1.xCenter;
+        this.y = a1.yCenter;
         this.w = 50;
         this.h = 10;
         this.color = "red";
@@ -122,6 +212,7 @@ class Cross {
         // rotate(45);
         rect(this.x, this.y, this.w, this.h);
         rect(this.x, this.y, this.h, this.w);
+        // image(xImage, this.x, this.y);
     }
 }
 
