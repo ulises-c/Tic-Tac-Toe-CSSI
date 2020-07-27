8372808b46390118dc0d631d1a39bd4556e57ac8
uli-oli <ez1ollie@yahoo.com>
2020-07-27 15:56:22 -0700

    removed even more repeated code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let a1, a2, a3, a4, a5, a6, a7, a8, a9, area_locations_list;
+let a1, a2, a3, a4, a5, a6, a7, a8, a9, area_locations_list, area_collide_list;
 let a1Collide = false;
 let a2Collide = false;
 let a3Collide = false;
@@ -95,8 +95,8 @@ function draw(){
 }
 
 function drawLocations(){
-    for(let i = 0; i <= area_locations_list.length; i++){
-        text(i, area_locations_list[i].xCenter, area_locations_list[i].yCenter)
+    for(let i = 0; i < area_locations_list.length; i++){
+        text(i+1, area_locations_list[i].xCenter, area_locations_list[i].yCenter)
     }
 }
 
@@ -113,88 +113,29 @@ function checkCollision() {
     a1Collide = collidePointRect(mouseX, mouseY, 0, 0, a1.x, a1.y);
     a2Collide = collidePointRect(mouseX, mouseY, a1.x, 0, a2.x, a2.y);
     a3Collide = collidePointRect(mouseX, mouseY, a2.x, 0, a3.x, a3.y);
-
     a4Collide = collidePointRect(mouseX, mouseY, 0, a1.y, a4.x, a4.y);
     a5Collide = collidePointRect(mouseX, mouseY, a4.x, a2.y, a5.x, a5.y);
     a6Collide = collidePointRect(mouseX, mouseY, a5.x, a3.y, a6.x, a6.y);
-
     a7Collide = collidePointRect(mouseX, mouseY, 0, a4.y, a7.x, a7.y);
     a8Collide = collidePointRect(mouseX, mouseY, a7.x, a5.y, a8.x, a8.y);
     a9Collide = collidePointRect(mouseX, mouseY, a8.x, a6.y, a9.x, a9.y);
 
-    let area_collide_list = [a1Collide, a2Collide, a3Collide, a4Collide, a5Collide, a6Collide, a7Collide, a8Collide, a9Collide];
+    area_collide_list = [a1Collide, a2Collide, a3Collide, a4Collide, a5Collide, a6Collide, a7Collide, a8Collide, a9Collide];
 
-    for(let i = 0; i <= area_locations_list.length; i++){
+    for(let i = 0; i < area_locations_list.length; i++){
         if(area_collide_list[i]){
             test_circle.display(area_locations_list[i].xCenter, area_locations_list[i].yCenter);
+            return area_collide_list[i];
         }
     }
-    // if(a1Collide){
-    //     test_circle.display(a1.xCenter, a1.yCenter);
-    //     return a1Collide;
-    // }
-    // if(a2Collide){
-    //     test_circle.display(a2.xCenter, a2.yCenter);
-    //     return a2Collide;
-    // }
-    // if(a2Collide){
-    //     test_circle.display(a3.xCenter, a3.yCenter);
-    //     return a3Collide;
-    // }
-    // if(a4Collide){
-    //     test_circle.display(a4.xCenter, a4.yCenter);
-    //     return a4Collide;
-    // }
-    // if(a5Collide){
-    //     test_circle.display(a5.xCenter, a5.yCenter);
-    //     return a5Collide;
-    // }
-    // if(a6Collide){
-    //     test_circle.display(a6.xCenter, a6.yCenter);
-    //     return a6Collide;
-    // }
-    // if(a7Collide){
-    //     test_circle.display(a7.xCenter, a7.yCenter);
-    //     return a7Collide;
-    // }
-    // if(a8Collide){
-    //     test_circle.display(a8.xCenter, a8.yCenter);
-    //     return a8Collide;
-    // }
-    // if(a9Collide){
-    //     test_circle.display(a9.xCenter, a9.yCenter);
-    //     return a9Collide;
-    // }
 }
 
 function mousePressed(){
-    if (a1Collide){
-        console.log("Area 1 collision: ", a1Collide);
-        test_cross.display();
-    }
-    if (a2Collide){
-        console.log("Area 2 collision: ", a3Collide);
-    }
-    if (a3Collide){
-        console.log("Area 3 collision: ", a3Collide);
-    }
-    if(a4Collide){
-        console.log("Area 4 collision: ", a4Collide);
-    }
-    if(a5Collide){
-        console.log("Area 5 collision: ", a5Collide);
-    }
-    if(a6Collide){
-        console.log("Area 6 collision: ", a6Collide);
-    }
-    if(a7Collide){
-        console.log("Area 7 collision: ", a7Collide);
-    }
-    if(a8Collide){
-        console.log("Area 8 collision: ", a8Collide);
-    }
-    if(a9Collide){
-        console.log("Area 9 collision: ", a9Collide);
+    for(let j = 0; j < area_collide_list.length; j++){
+        if(area_collide_list[j]){
+            console.log("Area", j+1, "collision: ", area_collide_list[j]);
+            test_cross.display();
+        }
     }
 }
 
