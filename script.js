351cde5bc8a7c4440484ab82f330c88c23d649f9
uli-oli <ez1ollie@yahoo.com>
2020-07-28 21:44:10 -0700

    renamed a variable

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let areaLocation, areaCollision;
+let quadrantInfo, areaCollision;
 let boxWidth, boxHeight;
 let testBool = false;
 let boardArray = [];
 
 function setup(){
     createCanvas(800, 800);
-    areaLocations();
+    quadrantInfos();
     aCircle = new Circle();
     aCross = new Cross();
     boxWidth = width * 1/3;
@@ -36,45 +36,50 @@ function drawTest(){
     // rotate(angleRotation);
     // // translate(width * 1/10 + width * 1/6, height * 1/10 - height * 7/17);
     // // rotate(45);
-    // anotherCross.display(areaLocation[1].xCenter, areaLocation[1].yCenter);
+    // anotherCross.display(quadrantInfo[1].xCenter, quadrantInfo[1].yCenter);
 }
 
 function boardPopulate(){
+    // 
     for(let i = 0; i < boardArray.length; i++){
         for(let j = 0; j < boardArray[i].length; j++){
             if(boardArray[i][j] == "circle"){
-                aCircle.display(areaLocation[i][j].xCenter, areaLocation[i][j].yCenter);
+                aCircle.display(quadrantInfo[i][j].xCenter, quadrantInfo[i][j].yCenter);
             }
             else if (boardArray[i][j] == "cross"){
-                aCross.display(areaLocation[i][j].xCenter, areaLocation[i][j].yCenter);
+                aCross.display(quadrantInfo[i][j].xCenter, quadrantInfo[i][j].yCenter);
             }
         }
     }
 }
 
-function areaLocations(){
-    areaLocation = [
+function quadrantInfos(){
+    // array that stores quadrant coordinates
+    quadrantInfo = [
         [
             {
                 // top left
                 x: width * 1/3,
                 y: height * 1/3,
                 xCenter: width * 1/6,
-                yCenter: height * 1/6
+                yCenter: height * 1/6,
+                shape: ""
             },
             {
                 // top middle
                 x: width * 2/3,
                 y: height * 1/3,
                 xCenter: width * 3/6,
-                yCenter: height * 1/6
+                yCenter: height * 1/6,
+                shape: ""
             },
             {
                 // top right
                 x: width,
                 y: height * 1/3,
                 xCenter: width * 5/6,
-                yCenter: height * 1/6
+                yCenter: height * 1/6,
+                shape: ""
             }
         ],
         [
@@ -84,20 +89,23 @@ function areaLocations(){
                 y: height * 2/3,
                 xCenter: width * 1/6,
                 yCenter: height * 3/6,
+                shape: ""
             },
             {
                 // center
                 x: width * 2/3,
                 y: height * 2/3,
                 xCenter: width * 3/6,
-                yCenter: height * 3/6
+                yCenter: height * 3/6,
+                shape: ""
             },
             {
                 // middle right
                 x: width,
                 y: height * 2/3,
                 xCenter: width * 5/6,
-                yCenter: height * 3/6
+                yCenter: height * 3/6,
+                shape: ""
             }
         ],
         [
@@ -106,35 +114,40 @@ function areaLocations(){
                 x: width * 1/3,
                 y: height,
                 xCenter: width * 1/6,
-                yCenter: height * 5/6
+                yCenter: height * 5/6,
+                shape: ""
             },
             {
                 // bottom middle
                 x: width * 2/3,
                 y: height,
                 xCenter: width * 3/6,
-                yCenter: height * 5/6
+                yCenter: height * 5/6,
+                shape: ""
             },
             {
                 // bottom right
                 x: width,
                 y: height,
                 xCenter: width * 5/6,
-                yCenter: height * 5/6
+                yCenter: height * 5/6,
+                shape: ""
             }
         ]
     ];
 }
 
 function writeNumbers(){
-    for(let i = 0; i < areaLocation.length; i++){
-        for(let j = 0; j< areaLocation[i].length; j++){
-            text(`${i}, ${j}`, areaLocation[i][j].xCenter, areaLocation[i][j].yCenter)
+    // writes numbers within quadrants
+    for(let i = 0; i < quadrantInfo.length; i++){
+        for(let j = 0; j< quadrantInfo[i].length; j++){
+            text(`${i}, ${j}`, quadrantInfo[i][j].xCenter, quadrantInfo[i][j].yCenter)
         }
     }
 }
 
 function ticTacToeBoard(){
+    // draws lines to create tic tac toe board
     fill("black");
     stroke(5);
     verticalLine1 = line(width * 1/3, 0, width * 1/3, height);
@@ -144,30 +157,32 @@ function ticTacToeBoard(){
 }
 
 function checkCollision(){
+    // checks collisions between quadrants and mouse
+    // used to determine where to place the circle or cross
     // collidePointRect(pointX, pointY, x, y, width, height)
     areaCollision = [
         // Row 0
         [
             collidePointRect(mouseX, mouseY, 0, 0, boxWidth, boxHeight),
-            collidePointRect(mouseX, mouseY, areaLocation[0][0].x, 0, boxWidth, boxHeight),
-            collidePointRect(mouseX, mouseY, areaLocation[0][1].x, 0, boxWidth, boxHeight)
+            collidePointRect(mouseX, mouseY, quadrantInfo[0][0].x, 0, boxWidth, boxHeight),
+            collidePointRect(mouseX, mouseY, quadrantInfo[0][1].x, 0, boxWidth, boxHeight)
         ],
         // Row 1
         [
-            collidePointRect(mouseX, mouseY, 0, areaLocation[0][0].y, boxWidth, boxHeight),
-            collidePointRect(mouseX, mouseY, areaLocation[1][0].x, areaLocation[0][0].y, boxWidth, boxHeight),
-            collidePointRect(mouseX, mouseY, areaLocation[1][1].x, areaLocation[0][2].y, boxWidth, boxHeight)
+            collidePointRect(mouseX, mouseY, 0, quadrantInfo[0][0].y, boxWidth, boxHeight),
+            collidePointRect(mouseX, mouseY, quadrantInfo[1][0].x, quadrantInfo[0][0].y, boxWidth, boxHeight),
+            collidePointRect(mouseX, mouseY, quadrantInfo[1][1].x, quadrantInfo[0][2].y, boxWidth, boxHeight)
         ],
         // Row 2
         [
-            collidePointRect(mouseX, mouseY, 0, areaLocation[1][0].y, boxWidth, boxHeight),
-            collidePointRect(mouseX, mouseY, areaLocation[2][0].x, areaLocation[1][1].y, boxWidth, boxHeight),
-            collidePointRect(mouseX, mouseY, areaLocation[2][1].x, areaLocation[1][2].y, boxWidth, boxHeight)
+            collidePointRect(mouseX, mouseY, 0, quadrantInfo[1][0].y, boxWidth, boxHeight),
+            collidePointRect(mouseX, mouseY, quadrantInfo[2][0].x, quadrantInfo[1][1].y, boxWidth, boxHeight),
+            collidePointRect(mouseX, mouseY, quadrantInfo[2][1].x, quadrantInfo[1][2].y, boxWidth, boxHeight)
         ]
     ];
 
-    for(let i = 0; i < areaLocation.length; i++){
-        for(let j = 0; j < areaLocation[i].length; j++){
+    for(let i = 0; i < quadrantInfo.length; i++){
+        for(let j = 0; j < quadrantInfo[i].length; j++){
             // the following updates the board
             // if collision is true in the quadrant
             // & if mouse is clicked
@@ -175,13 +190,13 @@ function checkCollision(){
             if(areaCollision[i][j] && mouseIsPressed && boardArray[i][j] == "empty"){
                 // the following updates boardArray
                 if(testBool){
-                    aCircle.display(areaLocation[i][j].xCenter, areaLocation[i][j].yCenter);
+                    aCircle.display(quadrantInfo[i][j].xCenter, quadrantInfo[i][j].yCenter);
                         boardArray[i][j] = "cross";
                     // returning cross because previous click was a cross
                     return "cross";
                 }
                 else if(!testBool){
-                    aCross.display(areaLocation[i][j].xCenter, areaLocation[i][j].yCenter);
+                    aCross.display(quadrantInfo[i][j].xCenter, quadrantInfo[i][j].yCenter);
                         boardArray[i][j] = "circle"
                     // returning circle because previous click was a circle
                     return "circle";
@@ -192,6 +207,7 @@ function checkCollision(){
 }
 
 function mousePressed(){
+    // checks if the mouse has been clicked
     if(testBool) testBool = false;
     else if(!testBool) testBool= true;
     console.log(boardArray);
@@ -208,8 +224,6 @@ class Cross {
         noStroke();
         fill(this.color);
         rectMode(CENTER);
-        angleMode(DEGREES);
-        // rotate(1);
         rect(xLocation, yLocation, this.w, this.h);
         rect(xLocation, yLocation, this.h, this.w);
         // image(crossImage, this.x, this.y);
